77581b7f164bc80d3b46a0e317edbd160c4d6308
Rasmus Anthin <scratty@gmail.com>
2024-03-31 21:21:34 +0200

    Adding compile flags c++2a and O3 to build_libsoundio_test.sh build script because we will need that soon.

--- build_libsoundio_test2.sh
+++ build_libsoundio_test2.sh
@@ -1,1 +1,1 @@
-g++ libsoundio_test2.cpp -o libsoundio_test2 -I/opt/homebrew/opt/libsoundio/include -L/opt/homebrew/opt/libsoundio/lib -lsoundio
+g++ libsoundio_test2.cpp -o libsoundio_test2 -I/opt/homebrew/opt/libsoundio/include -L/opt/homebrew/opt/libsoundio/lib -lsoundio -std=c++2a -O3
