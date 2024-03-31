3e5ce0e3c458c7d25c27aabd70b861e97da1df1c
Rasmus Anthin <scratty@gmail.com>
2024-03-31 13:29:30 +0200

    Adding test program supplied by libsoundio that plays a sine wave. I have copied it into two identical files right now libsoundio_test and libsoundio_test2. I intend to keep the first one intact as reference and to modifiy lisoundio_test2 so that it more and more resembles calls to the interface API. This will hopefully help me to work out the implementation details bottom-up instead of top-down. Future will tell if this is a good idea or not.

---
+++ build_libsoundio_test.sh
@@ -0,0 +1,1 @@
+g++ libsoundio_test.cpp -o libsoundio_test -I/opt/homebrew/opt/libsoundio/include -L/opt/homebrew/opt/libsoundio/lib -lsoundio
---
+++ build_libsoundio_test2.sh
@@ -0,0 +1,1 @@
+g++ libsoundio_test2.cpp -o libsoundio_test2 -I/opt/homebrew/opt/libsoundio/include -L/opt/homebrew/opt/libsoundio/lib -lsoundio
